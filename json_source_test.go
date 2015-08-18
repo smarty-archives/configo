fa826e0bfe8fbce1d460b8b7a6c2ac73e647ab49
Oliver, Jonathan <git@jonathanoliver.com>
2015-08-18 15:52:59 -0600

    JSON source can now read various kinds of values.

--- json_source_test.go
+++ json_source_test.go
@@ -15,89 +15,55 @@ func (this *JSONSourceFixture) TestParseMalformedJSONPanics() {
 	malformed := []byte(`{"key":"value",}}}`)
 	this.So(func() { FromJSONContent(malformed) }, should.Panic)
 }
-
 func (this *JSONSourceFixture) TestNonExistentSingleValue() {
-	source := FromJSONContent([]byte(`{}`))
-
-	values, err := source.Values("key")
-
-	this.So(values, should.BeEmpty)
-	this.So(err, should.Equal, KeyNotFoundError)
+	this.assertFailure(`{}`, "key", KeyNotFoundError)
 }
 
 func (this *JSONSourceFixture) TestReadSingleStringValue() {
-	source := FromJSONContent([]byte(`{"key":"value"}`))
-
-	values, err := source.Values("key")
-
-	this.So(values, should.Resemble, []string{"value"})
-	this.So(err, should.BeNil)
+	this.assertSuccess(`{"key":"value"}`, "key", "value")
 }
 func (this *JSONSourceFixture) TestReadMultipleStringValues() {
-	source := FromJSONContent([]byte(`{"key": [ "a", "b", "c" ] }`))
-
-	values, err := source.Values("key")
-
-	this.So(values, should.Resemble, []string{"a", "b", "c"})
-	this.So(err, should.BeNil)
+	this.assertSuccess(`{"key": [ "a", "b", "c" ] }`, "key", "a", "b", "c")
 }
 
 func (this *JSONSourceFixture) TestReadSingleNumericValue() {
-	source := FromJSONContent([]byte(`{"key":1234}`))
-
-	values, err := source.Values("key")
-
-	this.So(values, should.Resemble, []string{"1234"})
-	this.So(err, should.BeNil)
+	this.assertSuccess(`{"key":1234}`, "key", "1234")
 }
-func (this *JSONSourceFixture) TestReadMultipleNumericValues() {
-	source := FromJSONContent([]byte(`{"key":[1,2,3]}`))
-
-	values, err := source.Values("key")
-
-	this.So(values, should.Resemble, []string{"1", "2", "3"})
-	this.So(err, should.BeNil)
+func (this *JSONSourceFixture) TestReadMultileNumericValues() {
+	this.assertSuccess(`{"key":[1,2,3]}`, "key", "1", "2", "3")
 }
 
 func (this *JSONSourceFixture) TestReadSingleDecimalValue() {
-	source := FromJSONContent([]byte(`{"key":1234.5678}`))
-
-	values, err := source.Values("key")
-
-	this.So(values, should.Resemble, []string{"1234.5678"})
-	this.So(err, should.BeNil)
+	this.assertSuccess(`{"key":1234.5678}`, "key", "1234.5678")
 }
 func (this *JSONSourceFixture) TestReadMultipleDecimalValues() {
-	source := FromJSONContent([]byte(`{"key":[1.2, 3.4, 5.6]}`))
-
-	values, err := source.Values("key")
-
-	this.So(values, should.Resemble, []string{"1.2", "3.4", "5.6"})
-	this.So(err, should.BeNil)
+	this.assertSuccess(`{"key":[1.2, 3.4, 5.6]}`, "key", "1.2", "3.4", "5.6")
 }
 
 func (this *JSONSourceFixture) TestReadSingleBooleanValue() {
-	source := FromJSONContent([]byte(`{"key":true}`))
-
-	values, err := source.Values("key")
-
-	this.So(values, should.Resemble, []string{"true"})
-	this.So(err, should.BeNil)
+	this.assertSuccess(`{"key":true}`, "key", "true")
 }
 func (this *JSONSourceFixture) TestReadMultipleBooleanValues() {
-	source := FromJSONContent([]byte(`{"key":[true, false, true]}`))
-
-	values, err := source.Values("key")
-
-	this.So(values, should.Resemble, []string{"true", "false", "true"})
-	this.So(err, should.BeNil)
+	this.assertSuccess(`{"key":[true, false, true]}`, "key", "true", "false", "true")
 }
 
 func (this *JSONSourceFixture) TestReadMultipleValues() {
-	source := FromJSONContent([]byte(`{"key":["value", 1, 1.2, true]}`))
+	this.assertSuccess(`{"key":["value", 1, 1.2, true]}`, "key", "value", "1", "1.2", "true")
+}
 
-	values, err := source.Values("key")
+func (this *JSONSourceFixture) assertSuccess(raw, key string, expectedValues ...string) {
+	source := FromJSONContent([]byte(raw))
 
-	this.So(values, should.Resemble, []string{"value", "1", "1.2", "true"})
+	values, err := source.Values(key)
+
+	this.So(values, should.Resemble, expectedValues)
 	this.So(err, should.BeNil)
+}
+func (this *JSONSourceFixture) assertFailure(raw, key string, expectedError error) {
+	source := FromJSONContent([]byte(raw))
+
+	values, err := source.Values(key)
+
+	this.So(values, should.BeEmpty)
+	this.So(err, should.Equal, expectedError)
 }
