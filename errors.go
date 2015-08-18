a19da6fece079dee0f57e519ecd7304a525005bd
Whatcott, Michael <mdwhatcott@gmail.com>
2015-08-18 13:53:42 -0600

    Blank project.

--- errors.go
+++ errors.go
@@ -2,5 +2,7 @@ package newton
 
 import "errors"
 
-var KeyNotFoundError = errors.New("The specified key was not found.")
-var MalformedValueError = errors.New("The specified value could not be parsed.")
+var (
+	KeyNotFoundError    = errors.New("The specified key was not found.")
+	MalformedValueError = errors.New("The specified value could not be parsed.")
+)
