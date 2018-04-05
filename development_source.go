132a4a6976944cf6bc4a5ceeb3ae11e0827f60ac
Michael Whatcott <mdwhatcott@gmail.com>
2018-04-05 10:27:25 -0600

    Various refactorings:
    - converted var-style funcs into 1st-class funcs.
    - extracted functions for multi-return calls to allow short-circuiting.
    - Reformatted and re-spaced functions.

--- development_source.go
+++ development_source.go
@@ -6,26 +6,28 @@ import (
 	"runtime"
 )
 
-var developmentEnvironment = func() bool {
-	hostname, _ := os.Hostname()
-	vagrant, _ := user.Lookup("vagrant")
-	return runtime.GOOS == "darwin" ||
-		hostname == "vagrant" ||
-		vagrant != nil
-}
-
-var productionEnvironment = func() bool {
-	return !developmentEnvironment()
-}
-
-// A conditional source which determines if we are running
-// in a development environment
+// A conditional source which determines if we are running in a development environment.
 func FromDevelopmentOnlyDefaults(pairs ...DefaultPair) *ConditionalSource {
 	return NewConditionalSource(developmentEnvironment, pairs...)
+}
+func developmentEnvironment() bool {
+	return runtime.GOOS == "darwin" || hostname() == "vagrant" || hasVagrantUser()
+}
+func hostname() string {
+	name, _ := os.Hostname()
+	return name
+}
+func hasVagrantUser() bool {
+	vagrant, _ := user.Lookup("vagrant")
+	return vagrant != nil
 }
 
 // If we are running in a production (non-development) environment
 // the specified JSON filename is required; otherwise it's optional.
 func FromRequiredInProductionJSONFile(filename string) Source {
 	return FromConditionalJSONFile(filename, productionEnvironment)
+}
+
+func productionEnvironment() bool {
+	return !developmentEnvironment()
 }
